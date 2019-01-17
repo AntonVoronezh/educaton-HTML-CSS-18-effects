f992c9435de5e0eccc67af04b2a43566d17e6ebd
AntonVoronezh <pstut@mail.ru>
2019-01-17 20:23:37 +0300

    layers.forEach

--- parallax-mountains/main.js
+++ parallax-mountains/main.js
@@ -8,6 +8,10 @@ moveParallax();
       const initialX = window.innerWidth / 2 - e.pageX;
       const initialY = window.innerHeight / 2 - e.pageY;
   
+      layers.forEach((layer, i) => {
+        const divider = i / 50;
+  
+      });
     };
   
   }
