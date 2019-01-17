cd2031a291e10ca028a3139b94771259d257130a
AntonVoronezh <pstut@mail.ru>
2019-01-17 20:23:10 +0300

    layers

--- parallax-mountains/main.js
+++ parallax-mountains/main.js
@@ -2,5 +2,6 @@ moveParallax();
 
   function moveParallax() {
     const parallaxContainer = document.querySelector(".parallax");
+    const layers = Array.from(parallaxContainer.children);
   
   }
