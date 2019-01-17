c1f33e853ea5306dcb54e5a7ba6bbd6d67eb8522
AntonVoronezh <pstut@mail.ru>
2019-01-17 20:23:22 +0300

    moveLayers

--- parallax-mountains/main.js
+++ parallax-mountains/main.js
@@ -4,4 +4,10 @@ moveParallax();
     const parallaxContainer = document.querySelector(".parallax");
     const layers = Array.from(parallaxContainer.children);
   
+    const moveLayers = e => {
+      const initialX = window.innerWidth / 2 - e.pageX;
+      const initialY = window.innerHeight / 2 - e.pageY;
+  
+    };
+  
   }
