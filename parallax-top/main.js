91a316441a022fbc3df4f045f209a28628eed7e8
AntonVoronezh <pstut@mail.ru>
2019-01-19 19:43:54 +0300

    move

--- parallax-top/main.js
+++ parallax-top/main.js
@@ -10,5 +10,12 @@ function scrollParallax() {
     const img2 = document.querySelector(".img--2");
     const img3 = document.querySelector(".img--3");
   
+    return {
+      move: function(block, windowScroll, strafeAmount) {
+        let strafe = windowScroll / -strafeAmount + "%";
+        let transformString = `translate3d(0, ${strafe}, 0)`;
+  
 
+
+    };
   }
