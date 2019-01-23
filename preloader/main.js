573f85f0b25ff818e2f3490d581f2b110b73ac0d
AntonVoronezh <pstut@mail.ru>
2019-01-23 20:33:36 +0300

    add

---
+++ preloader/main.js
@@ -0,0 +1,17 @@
+(function() {
+
+
+    const preloader = document.querySelector('.preloader');
+    const rounds = document.querySelector('.preloader__img');
+    const progress = document.querySelector('.preloader__progress');
+
+
+
+
+
+    }
+    
+
+
+
+})();
