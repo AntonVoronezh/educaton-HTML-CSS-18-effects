40b43811cee153b7df0200bd4c56f4a5327563b2
AntonVoronezh <pstut@mail.ru>
2019-01-23 20:33:57 +0300

    images

--- preloader/main.js
+++ preloader/main.js
@@ -4,7 +4,10 @@
     const preloader = document.querySelector('.preloader');
     const rounds = document.querySelector('.preloader__img');
     const progress = document.querySelector('.preloader__progress');
-
+    const images = Array.from(document.querySelectorAll("img"));
+    const imagesCount = images.length;
+    const initStrokeDashOffset = 439;
+    let loadedImg = 0;
 
 
 
